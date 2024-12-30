9a26f01fedb852dc5369c281b0c480ee6baee2c6
L4XB <lukas.buck@e-mail.de>
2024-12-30 20:43:22 +0100

    core: added pokemon model & scrolling dependenci

--- lib/src/features/home/domain/pokemon_model.dart
+++ lib/src/features/home/domain/pokemon_model.dart
@@ -1,1 +1,14 @@
-class PokemonModel {}
+class PokemonModel {
+  final String id;
+  final String name;
+  final String imageURL;
+  final String type;
+  final double weight;
+
+  PokemonModel(
+      {required this.id,
+      required this.name,
+      required this.imageURL,
+      required this.type,
+      required this.weight});
+}
