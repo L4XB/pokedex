a89664dcca4b9d5c78b43ce91d279c1eb099c523
L4XB <lukas.buck@e-mail.de>
2024-12-30 23:02:58 +0100

    design: added some more type colors

--- lib/src/features/home/application/color_desicion.dart
+++ lib/src/features/home/application/color_desicion.dart
@@ -12,6 +12,20 @@ Color getColorForType(String type) {
       return const Color.fromARGB(255, 194, 179, 49).withOpacity(0.7);
     case 'bug':
       return const Color.fromARGB(255, 122, 194, 74).withOpacity(0.7);
+    case 'ground':
+      return const Color.fromARGB(255, 149, 119, 63).withOpacity(0.7);
+    case 'poison':
+      return const Color.fromARGB(255, 161, 103, 197).withOpacity(0.7);
+    case 'fairy':
+      return const Color.fromARGB(255, 236, 158, 239).withOpacity(0.7);
+    case 'fighting':
+      return const Color.fromARGB(255, 227, 159, 71).withOpacity(0.7);
+    case 'psychic':
+      return const Color.fromARGB(255, 168, 88, 224).withOpacity(0.7);
+    case 'ghost':
+      return const Color.fromARGB(109, 122, 100, 232).withOpacity(0.7);
+    case 'ice':
+      return const Color.fromARGB(255, 65, 234, 229).withOpacity(0.7);
     default:
       return Colors.grey.withOpacity(0.7);
   }
