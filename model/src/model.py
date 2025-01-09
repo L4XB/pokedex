25a1b4bbc5e3aae2ecede65a10725eab74d62faf
Lukas Buck <103962359+L4XB@users.noreply.github.com>
2025-01-09 09:56:32 +0100

    Update model.py

--- model/src/model.py
+++ model/src/model.py
@@ -11,7 +11,7 @@ class Model(nn.Module):
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, padding = 1)
         self.bn2 = nn.BatchNorm2d(64)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(in_channels = 64, out_channels = 128, kernel_size = 3, padding = 1)
         self.bn3 = nn.BatchNorm2d(128)
         self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
         self.bn4 = nn.BatchNorm2d(256)
