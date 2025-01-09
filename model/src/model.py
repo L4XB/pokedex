9d2bce1d2199239c1597a56514f2a300eb67bdc0
Lukas Buck <103962359+L4XB@users.noreply.github.com>
2025-01-09 09:56:16 +0100

    Update model.py

--- model/src/model.py
+++ model/src/model.py
@@ -9,7 +9,7 @@ class Model(nn.Module):
         # Convolutional layers with Batch Normalization
         self.conv1 = nn.Conv2d(in_channels = 3, out_channels = 32, kernel_size = 3, padding = 1)
         self.bn1 = nn.BatchNorm2d(32)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, padding = 1)
         self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
         self.bn3 = nn.BatchNorm2d(128)
